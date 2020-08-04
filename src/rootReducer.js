dce5848d07f49772977decb557e7d26438ffe0e8
David Aripin Wihardja <67503001+dvdlovelucy17466@users.noreply.github.com>
2020-08-04 19:28:23 +0700

    Add files via upload

---
+++ src/rootReducer.js
@@ -0,0 +1,39 @@
+import { combineReducers } = 'redux';
+
+function arrangeViewing(state, action) {
+  switch(action.type) {
+    case "ARRANGE_VIEWING":
+      const { model, id } = action.data;
+      return `${model} and ${id}`
+    default:
+      return ""
+  }
+}
+
+function requestInfo(state, action) {
+  switch(action.type) {
+    case "REQUEST_INFO":
+      const { model, id } = action.data;
+      return `${model} and ${id}`
+    default:
+      return ""
+  }
+}
+
+function buyVehicle(state, action) {
+  switch(action.type) {
+    case "BUY_VEHICLE":
+      const { model, id } = action.data;
+      return `${model} and ${id}`
+    default:
+      return false
+  }
+}
+
+const rootReducer = combineReducers({
+  arrangeViewing,
+  requestInfo,
+  buyVehicle
+});
+
+export default rootReducer;
